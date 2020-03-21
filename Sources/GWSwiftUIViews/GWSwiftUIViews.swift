9f446c60b905d218bc33d785defd21907be2c7ce
amasson42 <amasson@student.42.fr>
2020-03-21 18:40:49 +0100

    activityindicatorview

--- Sources/GWSwiftUIViews/GWSwiftUIViews.swift
+++ Sources/GWSwiftUIViews/GWSwiftUIViews.swift
@@ -1,18 +1,19 @@
 
 import SwiftUI
 
-struct ActivityIndicatorView: UIViewRepresentable {
+@available(iOS 13.0, macOS 10.15, *)
+public struct ActivityIndicatorView: UIViewRepresentable {
     
     @Binding var animating: Bool
     var style: UIActivityIndicatorView.Style
     
-    func makeUIView(context: Context) -> UIActivityIndicatorView {
+    public func makeUIView(context: Context) -> UIActivityIndicatorView {
         let view = UIActivityIndicatorView()
         view.style = self.style
         return view
     }
     
-    func updateUIView(_ uiView: UIViewType, context: Context) {
+    public func updateUIView(_ uiView: UIViewType, context: Context) {
         if animating {
             uiView.startAnimating()
         } else {
